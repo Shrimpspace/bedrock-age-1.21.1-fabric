f4b21903ec9fe39d486648caf7c733f126674495
Yang Leo <helloshrimp0705@gmail.com>
2025-07-09 21:28:35 +0800

    改变文件

---
+++ src/main/java/online/shrimpspace/mod/BedrockAgeClient.java
@@ -0,0 +1,10 @@
+package online.shrimpspace.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrockAgeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
